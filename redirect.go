2ee601d9a3902165690243ec9f5326540f67d7a7
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-02-03 00:29:00 -0800

    Added termination signal handling

--- redirect.go
+++ redirect.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"syscall"
+	"time"
+
+	"github.com/gentlemanautomaton/signaler"
 )
 
 func main() {
+	// Capture shutdown signals
+	shutdown := signaler.New().Capture(os.Interrupt, syscall.SIGTERM)
+
+	// Parse arguments and environment
 	var prefix string
 	flag.StringVar(&prefix, "prefix", "", "URL Prefix")
 	flag.Parse()
@@ -18,9 +27,28 @@ func main() {
 	if prefix == "" {
 		prefix = "/"
 	}
+
+	// Register the handler with the default muxer
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("%s%s?%s\n", prefix, r.URL.Path, r.URL.RawQuery)
-		http.Redirect(w, r, fmt.Sprintf("%s%s?%s", prefix, r.URL.Path, r.URL.RawQuery), 301)
+		location := fmt.Sprintf("%s%s?%s", prefix, r.URL.Path, r.URL.RawQuery)
+		fmt.Println(location)
+		http.Redirect(w, r, location, 301)
 	})
-	log.Fatal(http.ListenAndServe(":80", nil))
+
+	// Create a server (which will use the default muxer by default)
+	s := &http.Server{Addr: ":80"}
+
+	// Tell the server to stop gracefully when a shutdown signal is received
+	stopped := shutdown.Then(func() {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		s.Shutdown(ctx)
+	})
+
+	// Always cleanup and wait until the shutdown has completed
+	defer stopped.Wait()
+	defer shutdown.Trigger()
+
+	// Run the server and print the final result
+	log.Println(s.ListenAndServe())
 }
